f042cb81216af2a18ade4ca02c458d769c3b2136
chanhee-lee <clee643@gatech.edu>
2020-03-29 18:43:55 -0400

    radii logic and refactoring of histogram

---
+++ radii.js
@@ -0,0 +1,115 @@
+export function radii(movieMap) {
+    var movieArr = [];
+    var movieIntervalData = [];
+    // Select svg
+    var svg = d3.select('svg');
+    const MINUTE = 5; 
+ 	// Load tarantino wordcount dataset
+    var wordDate = d3.csv('tarantino.csv').then(function(dataset) {
+    	// Setup movie arrays 
+        movieMap.forEach(element => movieArr.push(element))
+        movieMap.forEach(element => {
+        	// Calculate in interval 
+        	var intervalData = [];
+        	for (var i = 0; i < 180; i += 5) {
+        		// Get profanity in interval 
+        		var start = i; 
+        		var end = i+5; 
+        		var counter = 0;
+        		// Oh god this is so inefficient so if there's time we'll find a better way to do this 
+        		for (var j = 0; j < dataset.length; j++) {
+        			if (dataset[j].movie === element.title && dataset[j].minutes >= start && dataset[j].minutes < end) {
+        				counter++;
+        			}
+        		}
+        		intervalData.push(counter);
+        	}
+        	movieIntervalData.push({"title": element.title, "intervalData": intervalData});
+        })
+        console.log(movieIntervalData);
+
+        var color = ["#ff0000", "#ff4400", "#ffff00", "#00ff00", "#0000ff", "#4b0082", "#7f00ff"]
+
+        var svg = d3.select('svg');
+
+        var selector = d3.select('body')
+            .append("select")
+            .attr("id", "filmselector")
+            .selectAll("option")
+            .data(movieArr)
+            .enter().append("option")
+            .text(function(d) {
+                return d.title;
+            })
+            .attr("value", function(d) {
+                return d.title;
+            })   
+
+	  	// For Each Movie: Make circle 
+	  	var inner = 10;
+	  	var outer = 20;
+	  	for (var i = 0; i < movieArr.length; i++) {
+		    // Make circles based on data 
+		    var arcInterval = new Array(); 	// 
+		    for (var j = 0; j < 36; j++) {
+		    	arcInterval.push(1);
+		    }
+			var data = movieIntervalData[i].intervalData;
+
+		    createArc(arcInterval, data, inner, outer, color[i]);
+		    inner += 20; 
+		    outer += 20;
+	  	}
+    })	
+}
+
+/**
+* Creates arc for whichever movie data is passed in 
+* arcInterval: Array used to set up the 180 / MINUTE (constant) minute intervals of the chart 
+* data: Array of same length as arcInterval. Values contain number of profanity used in each interval.
+* 		Used to calculate color affected by intensity 
+* innerRadius: Inner radius size of arc
+* outerRadius: Outer radius size of arc
+*/
+function createArc(arcInterval, data, innerRadius, outerRadius, color) {
+	var svg = d3.select("svg"),
+        width = svg.attr("width"),
+        height = svg.attr("height"),
+        radius = Math.min(width, height) / 4,
+        g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Generate the pie
+    var pie = d3.pie();
+
+    // Generate the arcs
+    var arc = d3.arc()
+                .innerRadius(innerRadius)
+                .outerRadius(outerRadius);
+
+    //Generate groups
+    var arcs = g.selectAll("arc")
+                .data(pie(arcInterval))
+                .enter()
+                .append("g")
+                .attr("class", "arc")
+
+    //Draw arc paths
+    arcs.append("path")
+        .attr("fill", function(d, i) {
+        	// Calculate color 
+        	return getColor(color, 5, data[i]);
+        })
+        .attr("d", arc);
+}
+
+/**
+* Gets color based on the ratio between current interval's profanity and total profanity 
+* base: Base color 
+* total: Total number of profanity in movie 
+* current: Number of profanity in current interval 
+*/
+function getColor(base, total, current) {
+	var c = d3.hsl(base);
+	c.s = current / total; 
+	return c; 
+}
