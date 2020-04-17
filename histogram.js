d1d940793a69f6b3c514d2abdcfa8a417c2a4324
Long <dlong42-gtri@csr-ast-l01.attlocal.net>
2020-04-17 11:04:47 -0400

    layout changes for histogram

--- histogram.js
+++ histogram.js
@@ -34,6 +34,11 @@ export function histogram() {
         update();
         
         function update() {
+            var svgWidth = parseInt(svg.style("width"))
+            console.log(svgWidth)
+            var svgHeight = parseInt(svg.style("height"))
+            console.log(svgHeight)
+
             var curMovie = dataset.filter(function(element) {
                 return element.movie == selectedMovie
             })
@@ -49,13 +54,9 @@ export function histogram() {
                 } else {
                     count = 1;
                 }
-                console.log(count)
                 curMovie[i].occurNum = count
-                console.log(curMovie[i].occurNum)
                 prevMinutes = curMinutes
             }
-        
-            console.log(curMovie)
         
             var xDomain = d3.extent(curMovie, function(d) {
                 return +d.minutes
@@ -63,17 +64,15 @@ export function histogram() {
         
             var xScale = d3.scaleLinear()
                 .domain(xDomain)
-                .range([100,500]);
+                .range([svgWidth / 4, svgWidth * .75]);
         
             var yDomain = d3.extent(curMovie, function(d) {
                 return +d.occurNum
             })
         
-            console.log(yDomain)
-        
             var yScale = d3.scaleLinear()
                 .domain(yDomain)
-                .range([250, 100]);
+                .range([svgHeight * (2 / 3), svgHeight * (1 / 3)]);
         
             var xAxis = d3.axisBottom(xScale)
         
@@ -81,63 +80,73 @@ export function histogram() {
         
             svg.append('g')
                 .attr('class', 'x axis')
-                .attr('transform', 'translate(0, 260)')
+                .attr('transform', 'translate(0, ' + (svgHeight * (2 / 3) + 10) + ')')
                 .call(xAxis)
             
             svg.append('g')
                 .attr('class', 'y axis')
-                .attr('transform', 'translate(80, 0)')
+                .attr('transform', 'translate(' + (svgWidth / 4 - 10) + ', 0)')
                 .call(yAxis)
 
             svg.append("rect")
-                .attr("x", 550)
-                .attr("y", 100)
+                .attr("x", (svgWidth * (7/8)) - 20)
+                .attr("y", (svgHeight / 8) - 7)
                 .attr("width", 7)
                 .attr("height", 7)
-                .style("fill", "black")
+                .style("fill", "white")
             
             svg.append("rect")
-                .attr("x", 550)
-                .attr("y", 120)
+                .attr("x", (svgWidth * (7/8)) - 20)
+                .attr("y", (svgHeight / 8) + 13)
                 .attr("width", 7)
                 .attr("height", 7)
                 .style("fill", "red")
 
             svg.append('text')
                 .attr('class', 'legend')
-                .attr('transform', 'translate(565,108)')
+                .attr('transform', 'translate(' + (svgWidth * (7/8)) + ',' + (svgHeight / 8) + ')')
                 .text('Word')
                 .attr('font-family', 'Arial')
-                .attr('font-size', '12');
+                .attr('font-size', '12')
+                .style("fill", "white");
 
             svg.append('text')
                 .attr('class', 'legend')
-                .attr('transform', 'translate(565,128)')
+                .attr('transform', 'translate(' + (svgWidth * (7/8)) + ',' + ((svgHeight / 8) + 20) + ')')
                 .text('Death')
                 .attr('font-family', 'Arial')
-                .attr('font-size', '12');
+                .attr('font-size', '12')
+                .style("fill", "white")
+                .style('fill', 'white')
 
             svg.append('text')
                 .attr('class', 'title')
-                .attr('transform', 'translate(190,50)')
+                .attr('transform', 'translate(' + (svgWidth / 2) + ',' + (svgHeight / 4) + ')')
                 .text('Tarantino Vulgarity Histogram')
                 .attr('font-family', 'Arial')
                 .attr('font-size', '16')
                 .attr('font-weight', 'bold')
+                .style('text-align', 'center')
+                .style('fill', 'white')
+                .style('text-anchor', 'middle')
 
             svg.append('text')
                 .attr('class', 'x label')
-                .attr('transform', 'translate(260,300)')
+                .attr('transform', 'translate(' + (svgWidth / 2) + ',' + (svgHeight * (2 / 3) + 50) + ')')
                 .text('Minutes into Film')
                 .attr('font-family', 'Arial')
-                .attr('font-size', '12');
+                .attr('font-size', '12')
+                .style('fill', 'white')
+                .style('text-anchor', 'middle')
 
             svg.append('text')
                 .attr('class', 'y label')
-                .attr('transform', 'translate(40,215) rotate(270)')
+                .attr('transform', 'translate(' + ((svgWidth / 4) - 50) + ',' + (svgHeight /2) + ') rotate(270)')
                 .text('Vulgarity Count')
                 .attr('font-family', 'Arial')
-                .attr('font-size', '12');
+                .attr('font-size', '12')
+                .style('fill', 'white')
+                .style('text-anchor', 'middle')
         
             var circle = svg.selectAll('circle')
                 .data(curMovie)
@@ -149,7 +158,7 @@ export function histogram() {
                 })
                 .attr('r', '3px')
                 .attr('fill', function(d) {
-                    return (d.type == 'word') ? 'black' : 'red'
+                    return (d.type == 'word') ? 'white' : 'red'
                 })
 
                 circle.enter()
@@ -162,7 +171,7 @@ export function histogram() {
                 })
                 .attr('r', '3px')
                 .attr('fill', function(d) {
-                    return (d.type == 'word') ? 'black' : 'red'
+                    return (d.type == 'word') ? 'white' : 'red'
                 })
                 
             circle.exit().remove()
